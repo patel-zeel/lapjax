429b1167fc02329afc5bd184bb7c199bc475266e
patel-zeel <patel_zeel@iitgn.ac.in>
2022-06-13 11:24:12 +0530

    fe changes

--- laplax/laplace.py
+++ laplax/laplace.py
@@ -46,7 +46,7 @@ class ADLaplace:
             assert param.ndim <= 1, "params must be scalar or vector"
         return params
 
-    def loss_fun(self, params, data, aux=None):
+    def loss_fn(self, params, data, aux=None):
         transformed_params = jax.tree_map(lambda param, bijector: bijector.forward(param), params, self.bijectors)
         prior_log_probs = jax.tree_map(
             lambda param, dist: dist.log_prob(param),
@@ -63,7 +63,7 @@ class ADLaplace:
         return loss
 
     def apply(self, params, data, aux=None):
-        precision = jax.hessian(self.loss_fun)(params, data, aux)
+        precision = jax.hessian(self.loss_fn)(params, data, aux)
         return Posterior(params, precision, self.bijectors)
 
 
@@ -77,37 +77,39 @@ class Posterior:
         self.size = jax.tree_map(lambda param: param.size, self.params)
         self.cumsum_size = np.cumsum(jax.tree_leaves(self.size))[:-1]
 
-    def untree_precision(self):
+    def untree_precision(self, keys):
         precision_matrix = []
-        for start in self.precision:
+        for start in keys:
             precision_matrix.append([])
-            for end in self.precision[start]:
+            for end in keys:
                 element = self.precision[start][end].reshape((self.size[start], self.size[end]))
                 precision_matrix[-1].append(element)
         return jnp.block(precision_matrix)
 
-    def get_normal_dist(self):
-        precision_matrix = self.untree_precision()
+    def get_normal_dist(self, keys):
+        precision_matrix = self.untree_precision(keys)
         covariance_matrix = jnp.linalg.inv(precision_matrix)
         loc = jnp.concatenate(jax.tree_map(jnp.atleast_1d, jax.tree_leaves(self.params)))
-        dist = tfd.MultivariateNormalFullCovariance(loc, covariance_matrix)
+        if len(loc) == 1:
+            dist = tfd.Normal(loc.ravel(), covariance_matrix.ravel()**0.5)
+        else:
+            dist = tfd.MultivariateNormalFullCovariance(loc, covariance_matrix)
         return dist
 
     def sample(self, seed, sample_shape=()):
-        dist = self.get_normal_dist()
+        dist = self.get_normal_dist(self.params.keys())
         normal_sample = dist.sample(sample_shape=sample_shape, seed=seed)
         split_sample = jnp.split(normal_sample, self.cumsum_size, axis=-1)
         split_sample = jax.tree_unflatten(self.guide, split_sample)
         return jax.tree_map(lambda sample, bijector: bijector.forward(sample), split_sample, self.bijectors)
 
     def log_prob(self, sample):
-        posterior_sample = jax.tree_map(jnp.atleast_1d, sample)
-        normal_sample = jax.tree_map(lambda sample, bijector: bijector.inverse(sample), posterior_sample, self.bijectors)
-        log_jacobian = jax.tree_map(lambda sample, bijector: jnp.atleast_1d(bijector.inverse_log_det_jacobian(sample)), posterior_sample, self.bijectors)
-        log_jacobian = jnp.squeeze(jax.vmap(lambda x: sum(jax.tree_leaves(x)))(log_jacobian))
+        normal_sample = jax.tree_map(lambda sample, bijector: bijector.inverse(sample), sample, self.bijectors)
+        log_jacobian = jax.tree_map(lambda samp, bijector: bijector.inverse_log_det_jacobian(samp), sample, self.bijectors)
+        log_jacobian = jax.vmap(lambda x: sum(jax.tree_leaves(x)))(log_jacobian)
         flat_sample = jnp.concatenate(jax.tree_map(jnp.atleast_1d, jax.tree_leaves(normal_sample)))
-        dist = self.get_normal_dist()
-        normal_log_prob = jnp.squeeze(jnp.atleast_1d(dist.log_prob(flat_sample)))
+        dist = self.get_normal_dist(sample.keys())
+        normal_log_prob = dist.log_prob(flat_sample)
         assert normal_log_prob.shape == log_jacobian.shape
         return normal_log_prob + log_jacobian
     
